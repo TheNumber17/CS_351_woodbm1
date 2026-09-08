34b6a8b74d25e2c579fb2cd353b64d942a8ed5f4
Brady Wood <woodbm1@sunypoly.edu>
2026-09-08 14:13:11 -0400

    feat: print request info

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    //TODO
+    console.log("** HTTP Request ******************");
+    console.log(`${req.method} ${req.url} HTTP/${req.httpVersion}`);
+    console.log("Headers:", req.headers);
 });
 
 server.listen(3000, () => {
