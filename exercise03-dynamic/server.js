0216bff3143ac1b9a34a2e8aa93a04e37afd780f
Brady Wood <woodbm1@sunypoly.edu>
2026-09-17 15:51:19 -0400

    feat: get homepage filepath

--- exercise03-dynamic/server.js
+++ exercise03-dynamic/server.js
@@ -1,14 +1,24 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
 
     if(req.method === "GET" && req.url === "/") {
+        const filepath = path.join(__dirname, "public", "index.html");
+        
+        fs.readFile(filepath, (err, data) => {
+            //TODO
+        });
+
         res.writeHead(200, { "Content-Type": "text/plain" });
         res.end("HOME");
+
         return;
     } else if(req.method === "GET" && req.url === "/story") {
         res.writeHead(200, { "Content-Type": "text/plain" });
         res.end("STORY");
+        
         return;
     }
 
